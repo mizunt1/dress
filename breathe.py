1ef7c9c5494a749935ac7a3b2ebd7e9ff9cce690
mizunt1 <mizunt@gmail.com>
2019-01-10 20:33:26 +0000

    works but dim

--- breathe.py
+++ breathe.py
@@ -1,74 +1,19 @@
 import RPi.GPIO as GPIO
 import time
-LedPin = 12
-LedPins = [12, 8, 5] 
-# pin numbers, not gpio numbers
-GPIO.setmode(GPIO.BOARD)
-# Numbers pins by physical location
-for LedPin in LedPins:
-    GPIO.setup(LedPin, GPIO.OUT)
 
-
-
-GPIO.output(LedPin, GPIO.LOW)  # Set pin to low(0V)
-
-# p = GPIO.PWM(LedPin, 1000)     # set Frequece to 1KHz
-# p.start(0)                     # Start PWM output, Duty Cycle = 0try:
-# try:
-#     while True:
-#         for dc in range(0, 101, 5):
-#             p.ChangeDutyCycle(dc)
-#             time.sleep(0.05)
-#         time.sleep(1)
-#         for dc in range(100, -1, -5):
-#             p.ChangeDutyCycle(dc)
-#             time.sleep(0.05)
-#         time.sleep(1)
-# except KeyboardInterrupt:
-#     p.stop()
-#     GPIO.output(LedPin, GPIO.HIGH)    # turn off all leds
-#     GPIO.cleanup()
-pins = LedPins
-p1 = GPIO.PWM(pins[0], 1000)
-p2 = GPIO.PWM(pins[1], 1000)
-p3 = GPIO.PWM(pins[2], 1000)
-p1.start(0)
-p2.start(0)
-p3.start(0)
-
-def glow_up(p1, p2, p3):
+def glow_up(pin_list):
     for dc in range(0, 101, 5):
-        p1.ChangeDutyCycle(dc)
-        p2.ChangeDutyCycle(dc)
-        p3.ChangeDutyCycle(dc)
+        for pin in pin_list:
+            pin.ChangeDutyCycle(dc)
         time.sleep(0.1)
-    return [p1, p2, p3]
+    return pin_list
 
-def glow_up_b(p1):
-    for dc in range(0, 101, 5):
-        p1.ChangeDutyCycle(dc)
+def glow_down(pins_list):
+    for dc in range(100, -1, -5):
+        for pin in pins_list:
+            pin.ChangeDutyCycle(dc)
         time.sleep(0.1)
-    return p1
-
-def glow_down(pins):
-    for dc in range(100, -1, -5):
-        pins[0].ChangeDutyCycle(dc)
-        pins[1].ChangeDutyCycle(dc)
-        pins[2].ChangeDutyCycle(dc)
-        time.sleep(0.2)
-    #pins[0].stop()
-    #pins[1].stop()
-    # pins[2].stop()
-    #GPIO.output(pin[0], GPIO.HIGH)
-    #GPIO.output(pin[1], GPIO.HIGH)
-    #GPIO.output(pin[2], GPIO.HIGH)
-    # GPIO.cleanup()
-
-def glow_down_b(pins):
-    for dc in range(100, -1, -5):
-        pins.ChangeDutyCycle(dc)
-        time.sleep(0.2)
-
+       
 if __name__ == '__main__':
     while True:
         p = glow_up(p1,p2,p3)
--- test.py
+++ test.py
@@ -2,41 +2,45 @@ from breathe import glow_up, glow_down, glow_up_b, glow_down_b
 import RPi.GPIO as GPIO   
 from sensor import measurement
 import time
-red_pins = [12, 8, 5]
-blue_pins = 29
+red_pins_num = [12, 8, 5]
+blue_pins_num =  [33] # 22, 33
 red_on = False
 blue_on = False
 GPIO.setmode(GPIO.BOARD)
 # Numbers pins by physical location
-for red_pin in red_pins:
+for red_pin in red_pins_num:
     GPIO.setup(red_pin, GPIO.OUT)
 
-#for blue_pin in blue_pins:
-#    GPIO.setup(blue_pin, GPIO.OUT)
-GPIO.setup(blue_pins, GPIO.OUT)
+for blue_pin in blue_pins_num:
+    GPIO.setup(blue_pin, GPIO.OUT)
 
-p1 = GPIO.PWM(red_pins[0], 1000)
-p2 = GPIO.PWM(red_pins[1], 1000)
-p3 = GPIO.PWM(red_pins[2], 1000)
+red_pins = []
+for red_pin in red_pins_num:
+    red_pin = GPIO.PWM(red_pin, 1000)
+    red_pins.append(red_pin)
 
-b1 = GPIO.PWM(blue_pins, 1000)
+blue_pins = []
+for blue_pin in blue_pins_num:
+    blue_pin = GPIO.PWM(blue_pin, 1000)
+    blue_pins.append(blue_pin)
 
-p1.start(0)
-p2.start(0)
-p3.start(0)
+for pin in red_pins:
+    pin.start(0)
 
-b1.start(0)
+for pin in blue_pins:
+    pin.start(0)
+
 try:
     while True:
         distance = measurement()
         print(distance)
         if distance > 30:
             if red_on is False:
-                red_objects = glow_up(p1,p2,p3)
+                red_objects = glow_up(red_pins)
                 red_on = True
                 print("dis more 30 red on")
             if blue_on is True:
-                glow_down_b(b1)
+                glow_down(blue_pins)
                 print("blue off")
                 blue_on = False
             
@@ -44,27 +48,31 @@ try:
             if red_on:
                 pass
             else:
-                red_objects = glow_up(p1,p2,p3)
+                red_objects = glow_up(red_pins)
                 red_on = True
                 print("dis middle red on")
             if blue_on:
                 pass
             else:
-                blue_objects = glow_up_b(b1)
+                blue_objects = glow_up(blue_pins)
                 print("blue on")
                 blue_on = True
             
         else:
             if red_on is True:
-                glow_down(red_objects)
+                glow_down(red_pins)
                 red_on = False
                 print("diss less 30, red off")
             if blue_on is False:
-                glow_up_b(b1)
+                glow_up(blue_pins)
                 blue_on = True
                 print("blue on")
     time.sleep(1.5)
 
 except KeyboardInterrupt:
     print("Measurement stopped by User")
+    for pin in red_pins:
+        pin.stop()
+    for pin in blue_pins:
+        pin.stop()
     GPIO.cleanup()
